c21f1dd50b9cee70b13099c083695991e9c2f81e
Ronak Patel <ronak.patel@dcpltechnology.com>
2026-04-02 19:51:03 +0530

    Updates

--- assets/EmptyState-BYiozOT7.js
+++ assets/EmptyState-BYiozOT7.js
@@ -1,4 +1,4 @@
-import{j as t}from"./index-HHYZRv0W.js";import{c as s}from"./Loader-DyR05VkM.js";/**
+import{j as t}from"./index-DapWJqoM.js";import{c as s}from"./Loader-Ca6QlP4s.js";/**
  * @license lucide-react v1.7.0 - ISC
  *
  * This source code is licensed under the ISC license.
